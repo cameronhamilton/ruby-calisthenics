872b23814212e4543320e58099ae7e30b2477ced
Cameron <cameron.a.hamilton@gmail.com>
2017-09-15 17:58:32 +0000

    hw2 complete

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -3,8 +3,17 @@ class Class
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
+
     class_eval %Q{
-      # YOUR CODE HERE
+     def #{attr_name}=(value)
+      
+        if !defined? @#{attr_name}_history
+          @#{attr_name}_history = Array.new
+        end
+      
+        @#{attr_name}_history << @#{attr_name}
+        @#{attr_name} = value
+      end
     }
   end
 end
--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,49 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    word = self.downcase.gsub(/[^a-z0-9]/i, '')
+    return word.reverse == word
   end
   def count_words
-    # your code here
+    words = self.downcase.gsub(/[^a-z0-9\s]/i, '').split(' ')
+    word_counter = Hash.new(0)
+    words.each do | word |
+      word_counter[word] += 1
+    end
+    return word_counter
   end
   def anagram_groups
-    # your code here
+    words = self.split(' ')
+    anagrams = Array.new
+    processed_indicies = Array.new
+    
+    x = 0
+    while x < words.length
+    
+      if processed_indicies.include?(x)
+        next
+      end
+      
+      anagram_group = Array.new
+      anagram_group.push(words[x])
+      
+      y = 0
+      while y < words.length
+      
+        if y != x 
+          word_x = words[x].chars.sort_by(&:downcase).join
+          word_y = words[y].chars.sort_by(&:downcase).join
+          if word_x == word_y
+            anagram_group.push(words[y])
+          end
+        end
+        y += 1
+      end
+      
+      anagrams.push(anagram_group)
+      x += 1
+    end
+    
+    return anagrams
   end
 end
 
